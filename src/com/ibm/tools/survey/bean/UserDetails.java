12edaea61a71b41bdfaa7fa672466154088d0cb9
suddutt1 <suddutt1@in.ibm.com>
2016-01-11 17:11:27 +0530

    Changed

---
+++ src/com/ibm/tools/survey/bean/UserDetails.java
@@ -0,0 +1,74 @@
+package com.ibm.tools.survey.bean;
+
+//This bean should be available in session with key 
+//LOGGED_IN_USER 
+public class UserDetails {
+
+	private String emailId;
+	private String password;
+	private String displayName;
+	private String role;
+	
+	public UserDetails()
+	{
+		super();
+	}
+
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+
+	/**
+	 * @param emailId the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	/**
+	 * @param displayName the displayName to set
+	 */
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
