d8062d94fdd541b49d39d907a87897eb5b8af3e6
suddutt1 <suddutt1@in.ibm.com>
2016-01-14 18:15:34 +0530

    modified:   WebContent/app/questionconfig.jsp 	modified:   WebContent/app/surveyconfig.jsp 	modified:   src/com/ibm/tools/survey/action/AssessmentConfigAction.java 	new file:   src/com/ibm/tools/survey/bean/MaturityIndicatorAccumulator.java 	new file:   src/com/ibm/tools/survey/bean/MaturityIndicatorInfoMap.java

--- src/com/ibm/tools/survey/action/AssessmentConfigAction.java
+++ src/com/ibm/tools/survey/action/AssessmentConfigAction.java
@@ -18,50 +18,59 @@ import com.ibm.app.web.frmwk.bean.ModelAndView;
 import com.ibm.app.web.frmwk.bean.ViewType;
 import com.ibm.tools.survey.bean.AssesmentDetails;
 import com.ibm.tools.survey.bean.MaturityIndicator;
+import com.ibm.tools.survey.bean.MaturityIndicatorAccumulator;
 import com.ibm.tools.survey.bean.UserDetails;
 import com.ibm.tools.survey.dbaccess.CachedReferenceDataStore;
 import com.ibm.tools.survey.dbaccess.SurveyConfigDAO;
 
 public class AssessmentConfigAction implements WebActionHandler {
 
-	private static final String INDICATORS_SESSION_KEY ="INDICATORS";
+	private static final String INDICATORS_SESSION_KEY = "INDICATORS";
+	private static final String INDICATOR_MAP_SESSION_KEY = "INDICATOR_MAP";
+	
+
 	@RequestMapping("loadExistingQuestionConfig.wss")
 	public ModelAndView loadExistingQuestionConfig(HttpServletRequest request,
 			HttpServletResponse response) {
 		ModelAndView mvObject = new ModelAndView(ViewType.JSP_VIEW);
 		Gson serializer = new GsonBuilder().create();
-		mvObject.addModel("principleList", CachedReferenceDataStore.getPrinciples());
-		mvObject.addModel("practiceMap",serializer.toJson(CachedReferenceDataStore.getPrincipleToPracticeMap()));
+		mvObject.addModel("principleList",
+				CachedReferenceDataStore.getPrinciples());
+		mvObject.addModel("practiceMap", serializer
+				.toJson(CachedReferenceDataStore.getPrincipleToPracticeMap()));
 		mvObject.addModel("levelList", CachedReferenceDataStore.getLevels());
 		mvObject.setView("app/questionconfig.jsp");
 		return mvObject;
 	}
+
 	@RequestMapping("saveQuestionDetails.wss")
 	public ModelAndView saveQuestionDetails(HttpServletRequest request,
 			HttpServletResponse response) {
 		ModelAndView mvObject = new ModelAndView(ViewType.AJAX_VIEW);
-		//Build the indicator
+		// Build the indicator
 		String practice = request.getParameter("practice");
 		String principle = request.getParameter("principle");
 		int level = Integer.parseInt(request.getParameter("maturityLevel"));
 		String questionId = getSafeString(request.getParameter("questionId"));
-		String indicatorText = request.getParameter("maturityIndicator"); 
-		MaturityIndicator indicator = new MaturityIndicator(principle, practice, level, indicatorText, "Web added ..Input to be taken", 0,questionId);
-		if((new SurveyConfigDAO()).updateMaturityIndicator(indicator))
-		{
+		String indicatorText = request.getParameter("maturityIndicator");
+		MaturityIndicator indicator = new MaturityIndicator(principle,
+				practice, level, indicatorText,
+				"Web added ..Input to be taken", 0, questionId);
+		if ((new SurveyConfigDAO()).updateMaturityIndicator(indicator)) {
 			Gson serializer = new GsonBuilder().create();
-			//if save succeeds then update the session object
-			Map<String,MaturityIndicator> indicatorMap =(Map<String,MaturityIndicator>) request.getSession().getAttribute(INDICATORS_SESSION_KEY);
-			indicatorMap.put(indicator.getQuestionid(),indicator);
-			mvObject.setView("{ \"status\": \"0\" , \"payload\" : "+serializer.toJson(indicator)+"}");
-		}
-		else
-		{
+			// if save succeeds then update the session object
+			Map<String, MaturityIndicator> indicatorMap = (Map<String, MaturityIndicator>) request
+					.getSession().getAttribute(INDICATORS_SESSION_KEY);
+			indicatorMap.put(indicator.getQuestionid(), indicator);
+			mvObject.setView("{ \"status\": \"0\" , \"payload\" : "
+					+ serializer.toJson(indicator) + "}");
+		} else {
 			mvObject.setView("{ \"status\": \"1\" }");
 		}
 
 		return mvObject;
 	}
+
 	@RequestMapping("getQuestionDetails.wss")
 	public ModelAndView getQuestionDetails(HttpServletRequest request,
 			HttpServletResponse response) {
@@ -71,85 +80,94 @@ public class AssessmentConfigAction implements WebActionHandler {
 		String principle = request.getParameter("principle");
 		int level = Integer.parseInt(request.getParameter("maturityLevel"));
 		String questionId = getSafeString(request.getParameter("questionId"));
-		//Map<String,List<>>
+		// Map<String,List<>>
 		Gson serializer = new GsonBuilder().create();
-		if(questionId.length()>0)
-		{
+		if (questionId.length() > 0) {
 			indicator = getExistingIndicator(questionId, request);
+		} else {
+			indicator = getExistingIndicator(principle, practice, level,
+					request);
 		}
-		else
-		{
-			indicator = getExistingIndicator(principle,practice,level, request);
-		}
-		if(indicator==null)
-		{
+		if (indicator == null) {
 			mvObject.setView("{ \"status\": \"1\" }");
-		}
-		else
-		{
-			mvObject.setView("{ \"status\" :  \"0\" , \"payload\" : "+serializer.toJson(indicator)+"}");
+		} else {
+			mvObject.setView("{ \"status\" :  \"0\" , \"payload\" : "
+					+ serializer.toJson(indicator) + "}");
 		}
 		return mvObject;
 	}
-	private MaturityIndicator getExistingIndicator(String principle,String practice,int level, HttpServletRequest request)
-	{
-		Map<String,MaturityIndicator> existingDataMap  =(Map<String,MaturityIndicator>) request.getSession().getAttribute(INDICATORS_SESSION_KEY);
-		if(existingDataMap==null)
-		{
+
+	private MaturityIndicator getExistingIndicator(String principle,
+			String practice, int level, HttpServletRequest request) {
+		Map<String, MaturityIndicator> existingDataMap = (Map<String, MaturityIndicator>) request
+				.getSession().getAttribute(INDICATORS_SESSION_KEY);
+		if (existingDataMap == null) {
 			existingDataMap = loadIndicators();
-			request.getSession().setAttribute(INDICATORS_SESSION_KEY,existingDataMap);
+			request.getSession().setAttribute(INDICATORS_SESSION_KEY,
+					existingDataMap);
 		}
-		for(MaturityIndicator indicator: existingDataMap.values())
-		{
-			if(indicator.isMatching(principle, practice, level))
-			{
+		for (MaturityIndicator indicator : existingDataMap.values()) {
+			if (indicator.isMatching(principle, practice, level)) {
 				return indicator;
 			}
 		}
 		return null;
 	}
-	private MaturityIndicator getExistingIndicator(String questionId,HttpServletRequest request)
-	{
-		Map<String,MaturityIndicator> existingDataMap  =(Map<String,MaturityIndicator>) request.getSession().getAttribute(INDICATORS_SESSION_KEY);
-		if(existingDataMap==null)
-		{
+
+	private MaturityIndicator getExistingIndicator(String questionId,
+			HttpServletRequest request) {
+		Map<String, MaturityIndicator> existingDataMap = (Map<String, MaturityIndicator>) request
+				.getSession().getAttribute(INDICATORS_SESSION_KEY);
+		if (existingDataMap == null) {
 			existingDataMap = loadIndicators();
-			request.getSession().setAttribute(INDICATORS_SESSION_KEY,existingDataMap);
+			request.getSession().setAttribute(INDICATORS_SESSION_KEY,
+					existingDataMap);
 		}
-		
+
 		return existingDataMap.get(questionId);
-		
+
 	}
-	private Map<String,MaturityIndicator> loadIndicators()
-	{
-		Map<String,MaturityIndicator> existingDataMap  = new LinkedHashMap<>();
-		List<MaturityIndicator> existingIndicators = (new SurveyConfigDAO()).getAllTypes(MaturityIndicator.TYPE, MaturityIndicator.class);
-		if(existingIndicators!=null )
-		{
-			for(MaturityIndicator indicator: existingIndicators)
-			{
-				existingDataMap.put(indicator.getQuestionid(),indicator);
+
+	private Map<String, MaturityIndicator> loadIndicators() {
+		Map<String, MaturityIndicator> existingDataMap = new LinkedHashMap<>();
+		List<MaturityIndicator> existingIndicators = (new SurveyConfigDAO())
+				.getAllTypes(MaturityIndicator.TYPE, MaturityIndicator.class);
+		if (existingIndicators != null) {
+			for (MaturityIndicator indicator : existingIndicators) {
+				existingDataMap.put(indicator.getQuestionid(), indicator);
 			}
 		}
 		return existingDataMap;
-				
+
 	}
+
 	@RequestMapping("loadAssesmentConfig.wss")
 	public ModelAndView loadConfig(HttpServletRequest request,
 			HttpServletResponse response) {
 		ModelAndView mvObject = new ModelAndView(ViewType.JSP_VIEW);
-		//Gson serializer = new GsonBuilder().create();
-		
+		// Gson serializer = new GsonBuilder().create();
+
 		// TODO: Change the above item
-		mvObject.addModel("principleMap", CachedReferenceDataStore.getAgilePrinciplesMap());
-		mvObject.addModel("practiceMap",CachedReferenceDataStore.getAgilePracticeMap());
-		mvObject.addModel("levelMap", CachedReferenceDataStore.getLevelsMap());
-		mvObject.addModel("indicatorList", (new SurveyConfigDAO().getAllTypes(MaturityIndicator.TYPE, MaturityIndicator.class)));
+		mvObject.addModel("principleMap",
+				CachedReferenceDataStore.getAgilePrinciplesMap());
+		mvObject.addModel("practiceMap",
+				CachedReferenceDataStore.getAgilePracticeMap());
+		mvObject.addModel("levels", CachedReferenceDataStore.getLevels());
+		MaturityIndicatorAccumulator accumulator = new MaturityIndicatorAccumulator();
 		
+		mvObject.addModel("indicatorList", accumulator
+				.getAccumulatedList((new SurveyConfigDAO().getAllTypes(
+						MaturityIndicator.TYPE, MaturityIndicator.class))));
+		request.getSession().setAttribute(INDICATOR_MAP_SESSION_KEY, accumulator);
 		mvObject.setView("app/surveyconfig.jsp");
 		return mvObject;
 	}
-
+	@RequestMapping("saveAssesmentDetails.wss")
+	public ModelAndView saveAssesmentDetails(HttpServletRequest request,HttpServletResponse response)
+	{
+		//ModelAndView 
+		return null;
+	}
 	private AssesmentDetails buildNewAssesment(HttpServletRequest request) {
 		String surveyName = request.getParameter("surveyName");
 		String releaseDate = request.getParameter("releaseDate");
@@ -162,10 +180,9 @@ public class AssessmentConfigAction implements WebActionHandler {
 				surveyName, releaseDate, comment);
 		return assesmentDetauls;
 	}
-	
-	private String getSafeString(String input)
-	{
-		return (input !=null ? input.trim(): "");
+
+	private String getSafeString(String input) {
+		return (input != null ? input.trim() : "");
 	}
 
 }
