3d8037e5c27f8f1844d884475e8545dc785299e2
rajuparashar23 <rajuparashar23@gmail.com>
2016-01-16 11:52:04 +0530

    added logout and glyphicons - raju

--- src/com/ibm/tools/survey/action/HomeAction.java
+++ src/com/ibm/tools/survey/action/HomeAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ibm.app.web.frmwk.WebActionHandler;
 import com.ibm.app.web.frmwk.annotations.RequestMapping;
@@ -18,34 +19,51 @@ import com.ibm.tools.survey.dbaccess.SurveyConfigDAO;
 public class HomeAction implements WebActionHandler {
 
 	@RequestMapping("home.wss")
-	public ModelAndView loadHome(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView loadHome(HttpServletRequest request,
+			HttpServletResponse response) {
 		ModelAndView mvObject = new ModelAndView(ViewType.JSP_VIEW);
 		mvObject.setView("app/newhome.jsp");
 		return mvObject;
 	}
 
 	@RequestMapping("gendashboard.wss")
-	public ModelAndView loadDashboard(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView loadDashboard(HttpServletRequest request,
+			HttpServletResponse response) {
 		UserDetails usrDetails = (UserDetails) request.getSession()
 				.getAttribute("LOGGED_IN_USER");
 		List<AssesmentDetails> filteredAssesmentDetails = new SurveyConfigDAO()
 				.getAssesmentsByOwner(usrDetails.getEmailId());
 
-        ModelAndView mvObject=new ModelAndView(ViewType.JSP_VIEW);
-        mvObject.addModel("assesmentDetails", filteredAssesmentDetails);
-        mvObject.setView("app/dashboard.jsp");
-        return mvObject;
-		
+		ModelAndView mvObject = new ModelAndView(ViewType.JSP_VIEW);
+		mvObject.addModel("assesmentDetails", filteredAssesmentDetails);
+		mvObject.setView("app/dashboard.jsp");
+		return mvObject;
+
 	}
-	
+
 	@RequestMapping("getmaturityscore.wss")
-	public ModelAndView showMaturityScore(HttpServletRequest request, HttpServletResponse response){
-		UserDetails usrDetails = (UserDetails) request.getSession().getAttribute("LOGGED_IN_USER");
-		String assesmentId=request.getParameter("assesmentId");
-		ModelAndView mvObject=new ModelAndView(ViewType.NO_VIEW);
-		List<AssessmentResult> listOfResults=new ResultScoreDAO().getResultAssesmentWise(assesmentId);
-		AjaxDataWriter<List<AssessmentResult>> writer= new AjaxDataWriter<>();
-		writer.write(response, listOfResults, (Class<List<AssessmentResult>>) listOfResults.getClass());
+	public ModelAndView showMaturityScore(HttpServletRequest request,
+			HttpServletResponse response) {
+		UserDetails usrDetails = (UserDetails) request.getSession()
+				.getAttribute("LOGGED_IN_USER");
+		String assesmentId = request.getParameter("assesmentId");
+		ModelAndView mvObject = new ModelAndView(ViewType.NO_VIEW);
+		List<AssessmentResult> listOfResults = new ResultScoreDAO()
+				.getResultAssesmentWise(assesmentId);
+		AjaxDataWriter<List<AssessmentResult>> writer = new AjaxDataWriter<>();
+		writer.write(response, listOfResults,
+				(Class<List<AssessmentResult>>) listOfResults.getClass());
+		return mvObject;
+	}
+
+	@RequestMapping("logout.wss")
+	public ModelAndView logout(HttpServletRequest request,
+			HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		ModelAndView mvObject = new ModelAndView(ViewType.JSP_VIEW);
+		session.removeAttribute("LOGGED_IN_USER");
+		session.invalidate();
+		mvObject.setView("index.jsp");
 		return mvObject;
 	}
 }
